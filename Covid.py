674dc3d8c9c804e857846495f8a38efcae4a3839
Chris Gamboa <cmgamboa@umich.edu>
2020-11-24 20:27:24 -0500

    Update Covid.py

--- Covid.py
+++ Covid.py
@@ -1,1 +1,11 @@
-
+import csv
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandas import DataFrame
+import numpy as np
+import seaborn as sns
+df = pd.read_json(r'https://data.cdc.gov/resource/9mfq-cb36.json')
+Stat = df[['state','tot_cases']].groupby(['state']).sum()
+print(Stat)
+Stat2 = df[['state','tot_death']].groupby(['state']).sum()
+print(Stat2)
