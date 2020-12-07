8739e9e0be3fe45053895e3dbe11d642b39404c5
cmgamboa <70605356+cmgamboa@users.noreply.github.com>
2020-12-07 00:39:53 -0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -62,9 +62,46 @@ def database(lst_tuple):
             #break
         print(g)
     print("done")
-    curr.execute('SELECT temperature FROM weather WHERE date BETWEEN date(2020-03-01) AND date(2020-03-31)'
-    print(cur.fetchall())
-    #return cur,conn
+    cur.execute("SELECT weather.temperature FROM weather WHERE temperature >= 0")
+    name_id = cur.fetchall()
+    march_lst = []
+    april_lst = []
+    may_lst = []
+    june_lst = []
+    avg_march = 0
+    sum_march = 0
+    avg_april = 0
+    sum_april = 0
+    avg_may = 0
+    sum_may = 0
+    avg_june = 0
+    sum_june = 0
+    for i in name_id[:31]:
+        march_lst.append(i[0])
+    for i in march_lst:
+        sum_march+=float(i)
+    for i in name_id[31:61]:
+        april_lst.append(i[0])
+
+    for i in april_lst:
+        sum_april+=float(i)
+    for i in name_id[61:89]:
+        may_lst.append(i[0])
+    for i in may_lst:
+        sum_may+=float(i)
+    for i in name_id[89:100]:
+        june_lst.append(i[0])
+    for i in june_lst:
+        sum_june+=float(i)
+    avg_march = sum_march / (len(march_lst))
+    avg_april = sum_april / (len(april_lst))
+    avg_may = sum_may / (len(may_lst))
+    avg_june = sum_june / (len(june_lst))
+    print(avg_march)
+    print(avg_april)
+    print(avg_may)
+    print(avg_june)
+    return cur,conn
 
 #def averages(cur,conn):
     #conn.execute("SELECT * FROM weather WHERE date = ?")
