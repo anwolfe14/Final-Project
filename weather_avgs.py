f98152654486b3960ac76946689d855299a45c6a
cmgamboa <70605356+cmgamboa@users.noreply.github.com>
2020-12-08 02:12:23 -0500

    Update weather_avgs.py

--- weather_avgs.py
+++ weather_avgs.py
@@ -40,6 +40,26 @@ for tup in name_id:
         may_lst.append(temperature)
     elif month == 6:
         june_lst.append(temperature)
+march_humid_lst = []
+april_humid_lst = []
+may_humid_lst = []
+june_humid_lst = []
+cur.execute("SELECT * FROM weather WHERE humidity >= 0")
+name_id = cur.fetchall()
+for tup in name_id:
+    date = tup[2]
+    date_lst = date.split('-')
+    temperature = float(tup[3])
+    month = int(date_lst[1])
+    if month == 3:
+        march_humid_lst.append(temperature)
+    elif month == 4:
+        april_humid_lst.append(temperature)
+    elif month == 5:
+        may_humid_lst.append(temperature)
+    elif month == 6:
+        june_humid_lst.append(temperature)
+
     
 # for i in name_id[:31]:
 #     march_lst.append(i[0])
@@ -66,6 +86,14 @@ print(avg_march)
 print(avg_april)
 print(avg_may)
 print(avg_june)
+avg_march_humid = sum(march_humid_lst) / (len(march_humid_lst))
+avg_april_humid = sum(april_humid_lst) / (len(april_humid_lst))
+avg_may_humid = sum(may_humid_lst) / (len(may_humid_lst))
+avg_june_humid = sum(june_humid_lst) / (len(june_humid_lst))
+print(avg_march_humid)
+print(avg_april_humid)
+print(avg_may_humid)
+print(avg_june_humid)
 #create a csv file that has headers as month names in temperature type
 # columns months and the averages 
 
@@ -73,15 +101,29 @@ print(avg_june)
 #pyplot can be easier to use 
 #add labels to x and y axis 
 # name of csv file  
-#filename = "weather_avgs.csv"
-
+filename = "weather_avgs.csv"
+title = ['Average monthly temperatures in fahrenheit for ']
+fields = ['March,' 'April,' 'May,' 'June']
+rows = ([avg_march],[avg_april],[avg_may],[avg_june])
 # writing to csv file  
-#with open(filename, 'w') as csvfile:  
+with open(filename, 'w') as csvfile:  
     # creating a csv writer object  
-    #csvwriter = csv.writer(csvfile)  
+    csvwriter = csv.writer(csvfile)  
         
     # writing the fields  
-    #csvwriter.writerow(fields)  
+    csvwriter.writerow(title)
+    csvwriter.writerow(fields)  
         
     # writing the data rows  
-    #csvwriter.writerows(rows) 
+    csvwriter.writerows(rows) 
+plt.title("Monthly Average Temperatures in Colorado ")
+plt.xlabel("Months")
+plt.ylabel("Temperature in Fahrenheit")
+dev_x = ['March','April','May','June']
+dev_y =[avg_march,avg_april,avg_may,avg_june]
+dev_y2 =[avg_march_humid,avg_april_humid,avg_may_humid,avg_june_humid]
+plt.plot(dev_x, dev_y, label='Temperatures')
+plt.plot(dev_x, dev_y2, 'k--', label='Humidity')
+plt.legend()
+plt.grid(True)
+plt.show()
